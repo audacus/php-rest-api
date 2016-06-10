77c8f20a3a8178e10067f76c9d13ea481a0766da
audacus <david.burkhart@hotmail.com>
2016-06-10 16:20:02 +0200

    -removed unused class

--- RestRequest.php
+++ RestRequest.php
@@ -7,8 +7,7 @@ class RestRequest {
 	private $parameters;
 
 	public function __construct() {
-		// echo '<pre>'.print_r($_SERVER,1).'</pre>';die();
-		$this->verb = $_SERVER['REQUEST_METHOD'];
+		$this->verb = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET';
 		$this->urlElements = explode('/', substr($_SERVER['PHP_SELF'], strlen($_SERVER['SCRIPT_NAME'])+1));
 		if (empty(end($this->urlElements))) {
 			array_pop($this->urlElements);
@@ -38,7 +37,7 @@ class RestRequest {
 		}
 
 		if (strpos($contentType, 'application/json') !== false) {
-			$bodyParams = json_decode($body, true);
+			$bodyParams = json_decode(json_encode(json_decode($body, true)));
 			if($bodyParams) {
 				foreach($bodyParams as $paramName => $paramValue) {
 					$parameters[$paramName] = $paramValue;
@@ -55,89 +54,31 @@ class RestRequest {
 			$this->format = 'html';
 		}
 
-		// switch($contentType) {
-		// 	case 'application/json':
-		// 		$bodyParams = json_decode($body);
-		// 		if($bodyParams) {
-		// 			foreach($bodyParams as $paramName => $paramValue) {
-		// 				$parameters[$paramName] = $paramValue;
-		// 			}
-		// 		}
-		// 		$this->format = 'json';
-		// 		break;
-		// 	case 'application/x-www-form-urlencoded':
-		// 		parse_str($body, $postvars);
-		// 		foreach($postvars as $field => $value) {
-		// 			$parameters[$field] = $value;
-
-		// 		}
-		// 		$this->format = 'html';
-		// 		break;
-		// 	default:
-		// 		// we could parse other supported formats here
-		// 		break;
-		// }
 		$this->parameters = $parameters;
 	}
 
-	/**
-	* Gets the value of urlElements.
-	*
-	* @return mixed
-	*/
 	public function getUrlElements() {
 		return $this->urlElements;
 	}
 
-	/**
-	* Sets the value of urlElements.
-	*
-	* @param mixed $urlElements the url elements
-	*
-	* @return self
-	*/
 	public function setUrlElements($urlElements){
 		$this->urlElements = $urlElements;
 		return $this;
 	}
 
-	/**
-	* Gets the value of verb.
-	*
-	* @return mixed
-	*/
 	public function getVerb() {
 		return $this->verb;
 	}
 
-	/**
-	* Sets the value of verb.
-	*
-	* @param mixed $verb the verb
-	*
-	* @return self
-	*/
 	public function setVerb($verb){
 		$this->verb = $verb;
 		return $this;
 	}
 
-	/**
-	* Gets the value of parameters.
-	*
-	* @return mixed
-	*/
 	public function getParameters() {
 		return $this->parameters;
 	}
 
-	/**
-	* Sets the value of parameters.
-	*
-	* @param mixed $parameters the parameters
-	*
-	* @return self
-	*/
 	public function setParameters($parameters){
 		$this->parameters = $parameters;
 		return $this;
